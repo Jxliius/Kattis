f8ce1fa280ba024a0c32d4840b6500ab044f354a
Jxliius <julius.reiter001@gmail.com>
2022-05-31 23:13:46 +0200

    Added Program Count the Vowels

---
+++ Count the Vowels/Count the Vowels/Count the Vowels/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Count_the_Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Eingabe = Convert.ToString(Console.ReadLine());
+            string Check = Eingabe.ToLower();
+            int count = 0;
+            //a e i o u
+            for(int i = 0; i < Check.Length; i++)
+            {
+                if(Check[i] == 'a' | Check[i] == 'e' | Check[i] == 'i' | Check[i] == 'o' | Check[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+           //This is a test. //4
+        }
+    }
+}

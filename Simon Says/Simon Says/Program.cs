7aa98a3655cf0e31e6db0934284b1ed6550b5842
Jxliius <julius.reiter001@gmail.com>
2022-05-31 19:21:54 +0200

    Added Problem Simon Says

---
+++ Simon Says/Simon Says/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Simon_Says
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anz = Convert.ToInt32(Console.ReadLine());
+            string[] sol = new string[anz];
+
+            for (int i = 0; i < anz; i++)
+            {
+                string Einlesen = Convert.ToString(Console.ReadLine());
+
+                if (Einlesen.StartsWith("Simon says"))
+                {
+                    sol[i] = Einlesen.Substring(11);
+                }
+            }
+            for(int i = 0; i < sol.Length; i++)
+            {
+                Console.WriteLine(sol[i]);
+            }
+        }
+    }
+}

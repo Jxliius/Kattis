d53990aa47772a3090ebfbae456f5950268bda61
Jxliius <julius.reiter001@gmail.com>
2022-06-01 09:57:33 +0200

    Added Problem Roaming Romans

---
+++ Roaming Romans/Roaming Romans/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Roaming_Romans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Mult = Convert.ToDouble(Console.ReadLine());
+
+            double Erg = 1000.0 * (5280.0 / 4854.0);
+            Erg *= Mult;
+            Console.WriteLine(Math.Round(Erg));   
+        }
+    }
+}

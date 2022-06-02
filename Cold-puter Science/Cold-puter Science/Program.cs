719c9a53b4cf536c265a7c0bf6dfa88106d6195d
Jxliius <julius.reiter001@gmail.com>
2022-06-02 11:04:20 +0200

    Added Problem Cold-puter Science

---
+++ Cold-puter Science/Cold-puter Science/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cold_puter_Science
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Anz = Convert.ToInt32(Console.ReadLine());
+            string Einlesen = Console.ReadLine();
+            string[]A =  Einlesen.Split(' ');
+            int count = 0;
+
+            for(int i = 0; i < A.Length; i++)
+            {
+                if (A[i].Contains("-"))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}

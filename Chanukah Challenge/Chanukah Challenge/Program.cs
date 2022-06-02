3164eba844c76294803c3b68bd00d163ee3bc08d
Jxliius <julius.reiter001@gmail.com>
2022-06-03 00:01:46 +0200

    added not finished program Chanukah Challenge

---
+++ Chanukah Challenge/Chanukah Challenge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Chanukah_Challenge
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            int anz = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
+

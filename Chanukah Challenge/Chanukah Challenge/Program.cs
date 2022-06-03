33a412f3531253d41fe233aa113fb7ec3df51b4d
Jxliius <julius.reiter001@gmail.com>
2022-06-03 08:07:20 +0200

    Finished Program Chanukah Challenge

--- Chanukah Challenge/Chanukah Challenge/Program.cs
+++ Chanukah Challenge/Chanukah Challenge/Program.cs
@@ -3,9 +3,32 @@ namespace Chanukah_Challenge
 {
     internal class Program
     {
-        static async void Main(string[] args)
+        static string Kalk(string text)
+        {
+            string[] Split = text.Split(' ');
+            int anz = Convert.ToInt32(Split[1]);
+            int sol = 0;
+            for (int i = 0; i < anz + 1; i++)
+            {
+                sol += i;
+            }
+            sol += anz;
+            return Split[0] + " " + sol;
+        }
+        static void Main(string[] args)
         {
             int anz = Convert.ToInt32(Console.ReadLine());
+            string[] solA = new string[anz];
+            string[] A = new string[anz];
+            for (int i = 0; i < anz; i++)
+            {
+                A[i] = Convert.ToString(Console.ReadLine());
+                solA[i] = Kalk(A[i]);
+            }
+            for (int i = 0; i < solA.Length; i++)
+            {
+                Console.WriteLine(solA[i]);
+            }
         }
 
     }

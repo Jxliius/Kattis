30274cc436ccad028bb230d53f7336ef9a93f436
Jxliius <julius.reiter001@gmail.com>
2022-06-07 21:43:09 +0200

    Added Problem SMIL

---
+++ SMIL/SMIL/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SMIL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Convert.ToString(Console.ReadLine());
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(i == input.Length - 1)
+                {
+                    break;
+                }
+                if (input[i] == ':' | input[i] == ';')
+                {
+                    if (input[i + 1] == ')')
+                    {
+                        Console.WriteLine(i);
+                    }
+                    else
+                    {
+                        if (input[i + 1] == '-')
+                        {
+                            if (input[i + 2] == ')')
+                            {
+                                Console.WriteLine(i);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

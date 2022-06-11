630672bb8edd38204e77b1ed55f098ad7bbd6dfe
Jxliius <julius.reiter001@gmail.com>
2022-06-11 21:34:58 +0200

    Added Problem Fifty Shades of Pink

---
+++ Fifty Shades of Pink/Fifty Shades of Pink/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fifty_Shades_of_Pink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anz = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            string input = string.Empty;
+            for(int i = 0; i < anz; i++)
+            {
+                input = Convert.ToString(Console.ReadLine());
+
+                if(input.ToLower().Contains("pink") | input.ToLower().Contains("rose"))
+                {
+                    count++;
+                }
+            }
+            if(count > 0)
+            {
+                Console.WriteLine(count);
+            }
+            else
+            {
+                Console.WriteLine("I must watch Star Wars with my daughter");
+            }
+        }
+    }
+}

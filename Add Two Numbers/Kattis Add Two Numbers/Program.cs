a47907db489db0a2898782be27d5faa1d5c41221
Jxliius <91068440+Jxliius@users.noreply.github.com>
2022-06-15 12:42:12 +0200

    Update Program.cs

--- Add Two Numbers/Kattis Add Two Numbers/Program.cs
+++ Add Two Numbers/Kattis Add Two Numbers/Program.cs
@@ -8,7 +8,7 @@ namespace Kattis_Add_Two_Numbers
         {
             string Einlesen = Convert.ToString(Console.ReadLine());
 
-            string[]A = Einlesen.Split(' ');
+            string[]A = Einlesen.Split(' ')
 
             int Erg = Convert.ToInt32(A[0]) + Convert.ToInt32(A[1]);
             Console.WriteLine(Erg);

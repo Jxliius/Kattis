ffd11e4b7d483ce654077b114cf6ff85b570c98b
Jxliius <julius.reiter001@gmail.com>
2022-09-16 21:54:15 +0200

    Added Problem Datum

---
+++ Datum/Datum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Datum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Einlesen = Convert.ToString(Console.ReadLine());
+
+            string[] split = Einlesen.Split(' ');
+            int Tag = Convert.ToInt32(split[0]);
+            int Monat = Convert.ToInt32(split[1]);
+
+            DateTime dateValue = new DateTime(2009, Monat, Tag);
+            Console.WriteLine(dateValue.ToString("dddd"));
+        }
+    }
+}

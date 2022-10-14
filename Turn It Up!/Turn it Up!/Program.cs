8d6f53e7465e0f5f332c1213892f183c04059085
Jxliius <julius.reiter001@gmail.com>
2022-10-14 08:41:15 +0200

    Added Problem Turn it up

---
+++ Turn It Up!/Turn it Up!/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Turn_it_Up_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Anzahl = Convert.ToInt32(Console.ReadLine());
+            int Wert = 7;
+            for (int i = 0; i < Anzahl; i++)
+            {
+                string Einlesen = Convert.ToString(Console.ReadLine());
+
+               if(Einlesen == "Skru op!")
+                {
+                    if(Wert == 10)
+                    {
+
+                    }
+                    else
+                    {
+                        Wert++;
+                    }
+                }
+                else
+                {
+                    if(Wert == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        Wert--;
+                    }
+                }
+            }
+            Console.WriteLine(Wert);
+        }
+    }
+}

bd521f3942fb8f6d977cdf0e6fda2641e763c51a
Jxliius <julius.reiter001@gmail.com>
2022-11-10 09:49:05 +0100

    Added Problem Oddities

---
+++ Oddities/Oddities/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Oddities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Anz = Convert.ToInt32(Console.ReadLine());
+            int[] Zahlen = new int[Anz];
+            for (int i = 0; i < Anz; i++)
+            {
+
+                Zahlen[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (int Zahl in Zahlen)
+            {
+                if (Math.Abs(Zahl) % 2 == 0)
+                {
+                    Console.WriteLine(Zahl + " is even");
+                }
+                else
+                {
+                    Console.WriteLine(Zahl + " is odd")
+                }
+            }
+        }
+    }
+}

c1d8fb78a89a86e042fc994b35da33e3b542135f
Jxliius <julius.reiter001@gmail.com>
2023-02-02 12:01:45 +0100

    Added Problem Sibice

---
+++ Sibice/Sibice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sibice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] split = input.Split(' ');
+            int matches = Convert.ToInt32(split[0]);
+            int width = Convert.ToInt32(split[1]);
+            int height = Convert.ToInt32(split[2]);
+            string[] answarr = new string[matches];
+            for(int i = 0; i < matches; i++)
+            {
+                int matchlength = Convert.ToInt32(Console.ReadLine());
+
+                if(matchlength <= Math.Sqrt(Math.Pow(width,2) + Math.Pow(height, 2)))
+                {
+                    answarr[i] = "DA";
+                }
+                else
+                {
+                    answarr[i] = "NE";
+                }
+            }
+
+            foreach(string s in answarr)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}

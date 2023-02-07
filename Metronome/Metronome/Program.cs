b0b7b4eae055f3dd87201287b6960f5ab59f1e49
Jxliius <julius.reiter001@gmail.com>
2023-02-07 15:10:57 +0100

    Added Problem Metronome

---
+++ Metronome/Metronome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metronome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert.ToInt32(Console.ReadLine()) / 4.0);
+        }
+    }
+}

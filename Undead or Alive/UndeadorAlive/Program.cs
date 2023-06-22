b99dbea6a94d96cc65d0ddc9735d908ce076ef77
Jxliius <julius.reiter001@gmail.com>
2023-06-22 12:46:19 +0200

    Added Problem Undead Or Alive

---
+++ Undead or Alive/UndeadorAlive/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UndeadorAlive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = "";
+
+            if (input.Contains(":)") && !input.Contains(":("))
+            {
+                output = "alive";
+            }
+            else if (input.Contains(":(") && !input.Contains(":)"))
+            {
+                output = "undead";
+            }
+            else if(input.Contains(":)") && input.Contains(":("))
+            {
+                output = "double agent";
+            }
+            else
+            {
+                output = "machine";
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
